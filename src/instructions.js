c2e2f1ea255e5eaa7d8e6d1604ee3fc4c73fe42a
Hans <zephyrfalcon@gmail.com>
2018-10-31 22:28:18 -0400

    added instructions

--- src/instructions.js
+++ src/instructions.js
@@ -6,8 +6,25 @@ import React from 'react';
 
 const instructions = (
     <div className="instructions">
-      <p>So yeah...</p>
-      <p>Like dis...</p>
+      <p>
+          This is a simple training game to learn <a 
+          href="https://en.wikipedia.org/wiki/Hiragana">hiragana</a> and/or <a 
+          href="https://en.wikipedia.org/wiki/Katakana">katakana</a>.
+      </p>
+      <p>
+          It works a bit like flash cards: a random kana symbol is displayed,
+          you type in the meaning and press Enter, and the game will tell you
+          if it was correct or not. If your guess was not correct, the right
+          answer will be displayed (for a short time). A simple scoreboard
+          will be updated after each answer.
+      </p>
+      <p>
+          <strong>Please note:</strong> There is <strong>no</strong> account or login,
+          and scores are <strong>not</strong> saved for later. In fact, they
+          are reset when you refresh the page. This is more of a simple way to
+          train yourself than a game.
+      </p>
+      <hr />
       <p>Sources:
           <ul>
               <li><a href="https://en.wikipedia.org/wiki/Katakana">Katakana table</a></li>
